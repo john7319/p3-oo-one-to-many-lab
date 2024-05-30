b85265784a47554ce816fcfd30668ca7f7b3ead9
JohnKaranja <jonniekaras@gmail.com>
2024-05-30 19:42:00 +0300

    Passed tests

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,27 @@
 class Pet:
-    pass
+    all = []
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    def __init__(self, name, pet_type, owner= None):
+        self.name = name 
+        self.owner = owner
+        if pet_type not in Pet.PET_TYPES:
+            raise ValueError("Input a valid Dog")
+        else:
+            self.pet_type= pet_type
+        Pet.all.append(self)
+        if owner:
+            owner.add_pet(self)
+        
+    
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets =[]
+    def add_pet(self, pet):
+        self._pets.append(pet)
+        pet.owner = self
+    def pets(self):
+        return self._pets
+    def get_sorted_pets(self):
+        return sorted(self._pets, key = lambda pet: pet.name)
